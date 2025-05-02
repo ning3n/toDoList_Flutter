9916ed23be4e7955e06e332f8446935835197a7f
Miguel <ningenmr@gmail.com>
2025-05-02 17:09:08 -0400

    First screen test code

--- lib/screens/lista_tareas_screen.dart
+++ lib/screens/lista_tareas_screen.dart
@@ -11,9 +11,55 @@ class _ListaTareasState extends State<ListaTareas> {
   final List<String> _tareas = [];
   final TextEditingController _controller = TextEditingController();
 
+  void _agregarTarea(){
+    if (_controller.text.isNotEmpty){
+      setState(() {
+        _tareas.add(_controller.text);
+        _controller.clear();
+      });
+    }
+  }
+
+  void _eliminarTarea(int index){
+    setState(() {
+      _tareas.removeAt(index);
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Lista de Tareas'),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Column(
+          children: [
+            TextField(
+              controller: _controller,
+              decoration: InputDecoration(
+                labelText: 'Escribe una tarea:',
+                border: OutlineInputBorder(),
+              ),
+            ),
+            SizedBox(height: 10),
+            ElevatedButton(onPressed: _agregarTarea, child: Text('Agregar')),
+            SizedBox(height: 20),
+            Expanded(
+              child: ListView.builder(
+                itemCount: _tareas.length,
+                itemBuilder: (context, index){
+                  return ListTile(
+                    title: Text(_tareas[index]),
+                    trailing: IconButton(icon: Icon(Icons.delete), onPressed: () => _eliminarTarea(index))
+                  );
+                }
+                ),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
